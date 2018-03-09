b28df0099727230ab1a743dee72ca69130fa1d05
Oczane <oczane.rivera@gmail.com>
2018-03-08 21:33:58 -0500

    Add google signin button and styling

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path')
 const PORT = (process.env.PORT || 4001)
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '.')))
 
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, './index.html'))
 })
 
 // server object respresenting the server
